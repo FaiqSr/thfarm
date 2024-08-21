733868cdadeb13bc6b28e40281473cc0009f0371
FaiqSr <FaiqSr@github.com>
2024-08-21 23:14:52 +0700

    add NavbarComponent to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import NavbarComponent from "./component/NavbarComponent";
 
 function App() {
   return (
     <div>
+      <NavbarComponent />
       <Routes>
         <Route path="/" Component={HomePage} />
       </Routes>
--- src/component/NavbarComponent.jsx
+++ src/component/NavbarComponent.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function NavbarComponent() {
-  return <div>NavbarComponent</div>;
+  return (
+    <div>
+      <h1 className="text-yellow-600">haha</h1>
+    </div>
+  );
 }
 
 export default NavbarComponent;
