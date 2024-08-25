eb34c5d23c397e2df3eb00cf6e39c8ee1568870b
FaiqSr <FaiqSr@github.com>
2024-08-25 20:31:13 +0700

    biar warna di tailwind bisa kepake

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      "white-utama": "#f4f9f4",
-      primary: "#a7d7c5",
-      second: "#74b49b",
-      third: "#5c8d89",
+    extend: {
+      colors: {
+        "white-utama": "#f4f9f4",
+        primary: "#a7d7c5",
+        second: "#74b49b",
+        third: "#5c8d89",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
