78b10431de700e81b4733df0899fc11c105ec027
FaiqSr <FaiqSr@github.com>
2024-08-25 13:28:06 +0700

    add apa gatau

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -40,7 +40,7 @@ function HomePage() {
             aspernatur. Repudiandae dolore ut animi consequuntur commodi
             veritatis porro, doloribus nulla vitae eius?
           </p>
-          <button className=" rounded-lg px-5 py-2 border text-lg px-10">
+          <button className=" rounded-lg  py-2 border text-lg px-10">
             Hubungi
           </button>
         </div>
@@ -54,9 +54,32 @@ function HomePage() {
         <div>
           <h1 className="text-3xl my-5">Apa si kata orang?</h1>
         </div>
-        <div className="mx-auto gap-16 columns-2 ">
+        <div className="mx-auto gap-16 columns-2 pb-10">
           <div className="container w-96 h-80 bg-second rounded-lg border"></div>
           <div className="container w-96 h-80 bg-second rounded-lg border"></div>
+        </div>
+      </section>
+
+      <section className="container border py-5  mx-auto my-24 flex-col max-w-5xl">
+        <div className="mx-10 px-10 py-5 ">
+          <h1 className="text-3xl pb-2">Brocademy</h1>
+          <p className="text-lg pb-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit eius
+            explicabo in voluptates! Saepe obcaecati aliquam explicabo
+            repellendus deleniti dignissimos rerum exercitationem, perspiciatis,
+            adipisci tempora molestias ratione aperiam eum quam voluptates
+            consequatur placeat natus accusantium veritatis, doloribus
+            aspernatur. Repudiandae dolore ut animi consequuntur commodi
+            veritatis porro, doloribus nulla vitae eius?
+          </p>
+          <button className=" rounded-lg  py-2 border text-lg px-10">
+            Hubungi
+          </button>
+        </div>
+        <div className="container flex flex-row justify-center gap-7 px-5 py-5">
+          <div className="container w-60 h-80 bg-second rounded-lg border"></div>
+          <div className="container w-60 h-80 bg-second rounded-lg border"></div>
+          <div className="container w-60 h-80 bg-second rounded-lg border"></div>
         </div>
       </section>
     </div>
