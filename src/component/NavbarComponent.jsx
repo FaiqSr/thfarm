fa32b93ae9e5d7bfbc368a1077631e80d6ce9fe7
FaiqSr <FaiqSr@github.com>
2024-08-26 21:07:23 +0700

    configurasi navbar

--- src/component/NavbarComponent.jsx
+++ src/component/NavbarComponent.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Search, Menu } from "react-feather";
 
 function NavbarComponent() {
-  return;
+  return (
+    <div className="fixed w-full h-16 justify-around top-0 flex bg-white-utama shadow-sm ">
+      <nav className="mx-auto flex items-center w-full justify-around">
+        <div>
+          <Search />
+        </div>
+        <div>
+          <a className="text-2xl" href="/">
+            BuBro Farm
+          </a>
+        </div>
+        <div>
+          <Menu />
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default NavbarComponent;
