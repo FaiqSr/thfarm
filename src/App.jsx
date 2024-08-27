26c368c86c0ac6c69c1200360d6c22432f5d30f1
FaiqSr <FaiqSr@github.com>
2024-08-27 23:44:12 +0700

    nambahin route aboutpage

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import NavbarComponent from "./component/NavbarComponent";
 import FooterComponent from "./component/FooterComponent";
+import AboutPage from "./pages/AboutPage";
+
 function App() {
   return (
     <div>
       <NavbarComponent />
       <Routes>
         <Route path="/" Component={HomePage} />
+        <Route path="/about" Component={AboutPage} />
       </Routes>
       <FooterComponent />
     </div>
