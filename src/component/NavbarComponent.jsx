fffb87970982a591a5d7db37863f27472fb8470e
FaiqSr <FaiqSr@github.com>
2024-08-29 15:51:18 +0700

    scroll event

--- src/component/NavbarComponent.jsx
+++ src/component/NavbarComponent.jsx
@@ -1,8 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Search, Menu } from "react-feather";
 
 function NavbarComponent() {
   const [open, setOpen] = useState("-right-96 w-96");
+
+  const [changeColor, setChangeColor] = useState(false);
+
+  const backgroundColorChange = () => {
+    if (window.scrollY > 10) {
+      setChangeColor(true);
+    } else {
+      setChangeColor(false);
+    }
+  };
+
+  useEffect(() => {
+    backgroundColorChange();
+
+    window.addEventListener("scroll", backgroundColorChange);
+  });
 
   function handleNavbar() {
     if (open === "-right-96 w-96") {
@@ -14,14 +30,22 @@ function NavbarComponent() {
 
   return (
     <div>
-      <div className="fixed w-full h-16 justify-around top-0 flex bg-white-utama shadow-sm ">
-        <nav className="mx-auto flex items-center w-full justify-around">
+      <div
+        className={`fixed w-full h-16 justify-around top-0 flex ${
+          changeColor ? "bg-white-utama shadow-sm p-10" : ""
+        } transition-all z-10`}
+      >
+        <nav className="mx-auto flex items-center w-full justify-around ">
           <div>
             <Search />
           </div>
           <div>
             <a className="text-2xl" href="/">
-              BuBro Farm
+              <img
+                src="../../public/logo/Oranye_Hitam_Minimalis_Pecel_Lele_Logo_3_-removebg-preview.png"
+                alt="bubro"
+                className="w-56 h-56"
+              />
             </a>
           </div>
           <div>
@@ -32,7 +56,9 @@ function NavbarComponent() {
         </nav>
       </div>
       <div
-        className={` mx-auto flex fixed  flex-col items-center justify-start bg-white-utama border h-full w-96 top-16 ${open} gap-10 transition-all `}
+        className={` mx-auto flex fixed  flex-col items-center justify-start  h-full w-96 top-16 ${open} gap-10 transition-all  ${
+          changeColor ? "bg-primary pt-5 " : ""
+        } `}
       >
         <div className="flex flex-col justify-center gap-4 my-2">
           <a href="about">About</a>
