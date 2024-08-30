ac0ae76698b2184fdd2621954c03eb2d1d202e03
FaiqSr <FaiqSr@github.com>
2024-08-30 20:24:07 +0700

    market mah sedikit

--- src/pages/MarketplacePage.jsx
+++ src/pages/MarketplacePage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 function MarketplacePage() {
-  return <div>MarketplacePage</div>;
+  return (
+    <>
+      <header className="min-h-96 flex justify-center items-center">
+        <div className="container bg-primary w-full h-60 "></div>
+      </header>
+      <div></div>
+    </>
+  );
 }
 
 export default MarketplacePage;
